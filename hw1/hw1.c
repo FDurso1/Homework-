09dd3703afb196ddfe00898168e238ea0e66008d
Francis <fdurso1@jh.edu>
2021-02-07 11:34:20 -0500

    All tests passed. Some uneeded testing comments remain. Error statements not exact. Edit later and submit to gradescope for next test.

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -15,11 +15,14 @@ int main(void) {
   int totalInput = 2; //a check for proper user input
   char mathSym = '+'; //a way to determine whether input was meant to be negative
   int isInput = 0; //This is for the specific case of no user input:
-
+  int endingSym = 0; //This is for detecting whether the user ends on a math symbol
+  
   printf("Please enter the weather trend using + and 0 only:\n");
   
   while ((totalInput = scanf(" %f %c", &temp, &tempType)) == 2) {
     isInput = 1; //there was at least some input detected.
+    
+    // printf("1.The current temp being calculated / added is: %f\n", temp);
     
     switch (tempType) {
       case 'c':
@@ -35,6 +38,8 @@ int main(void) {
         break;
     }
     
+    // printf("2.The middle temp being calculated is: %f\n", temp);
+    
     if (mathSym == '-' && temp > 0) {
       temp = -1 * temp; //for when the user inputs '- ' before the temperature
     }
@@ -43,19 +48,36 @@ int main(void) {
       printf("malformed expression\n");
       return 1;
     }
+
+    // printf("3.The final temp that is about to be added is: %f\n", temp);
     
     totalTemp += temp;
-    scanf(" %c", &mathSym); //the next + or - symbol (if any) is detected. A null works too
+    endingSym =  scanf(" %c", &mathSym);
+   //the next + or - symbol (if any) is detected. A null provides -1, the end of the input
+   // printf("The value of the mathSym is %d\n", mathSym);
+   // printf("The existence of the mathsym is %d\n", endingSym);
+    
   }
   
-  if (totalInput != 2 && (totalInput != -1 || isInput == 0)) {
-    //This will run under two conditions:
-    //The totalInput detected is not 2 (ie, there is not a number and a char and either:
-    //The totalInput is not -1  (which stops  pressing enter mid-input from ruining the code
-    //Or: there was no user input to begin with
-    printf("malformed expression\n");
-    return 1;
+  if (totalInput != 2) {
+    if (totalInput != -1) {
+      printf("malformed1 - there was not two expected values\n");
+      return 1;
+    }
+    else if (isInput == 0) {
+      printf("malformed2 - there was no user input");
+      return 1;
+    }
+    else if (endingSym != -1) {
+      printf("The user ended with a math symbol");
+      return 1;
+    }
   }
+    //This will run under three conditions:
+    //The totalInput detected is not 2 (ie, there is not a number and a char AND either:
+    //The totalInput is not -1  (which stops pressing enter mid-input from ruining the code
+    //Or: there was no user input to begin with
+    //Or, the user ended with a math symbol (+ or -)
   
   printf("%.2f F\n", totalTemp);
   return 0;
