05815b9b85c7edc4b91e6010e20de13264fea5b7
Francis <fdurso1@jh.edu>
2021-02-20 17:47:26 -0500

    almost have reading from a file down, error in that the example input.txt does not end in a newline character.

--- hw3/functions.c
+++ hw3/functions.c
@@ -5,17 +5,15 @@
 
 int read_file(FILE *fp, char words[][MAX_WORD_SIZE + 1], int size) {
   printf("read_file start\n");
-  char cur = '0';
+  char cur[] = NULL;
+  
   for (int i = 0; i < size; i++) {
-    printf("i: %d\n", i);
+    printf("Word %d:\n", i);
     int j = 0;
-    while ((cur = fscanf(fp, " %c", &cur) != '\n')) {
-	printf("j: %d, cur: %c\n", j, cur);
+    while ((fscanf(fp, "%s", &cur) != 0) {
+	printf("%c", cur);
 	words[i][j] = cur;
 	j++;
-	if (j > 40) {
-	  return -1;
-	}
     }
   }
   return 0;
--- hw3/hw3.c
+++ hw3/hw3.c
@@ -14,20 +14,30 @@ int main(int argc, const char *argv[]) {
   char words[ MAX_WORD_NUM ][ MAX_WORD_SIZE + 1 ]; 
     // TODO: Implement me!
   printf("main\n");
-  printf("argc: %d\n", argc);
+  if (argc !=2) {
+    printf("specify a file name");
+    return 1;
+  }
+  // printf("argc: %d\n", argc);
   printf("argv[1] = %s\n", argv[1]);
-  FILE* fptr;
-  fptr = fopen("input.txt", "w");
-  fprintf(fptr, "5\nintermediate\ninttterrmeediateeee\nprogram\nppproograaamm\naaa");
-  fclose(fptr);
+  
+  FILE *fptr = NULL;
+  fptr = fopen(argv[1], "r");
+  
+  //  fprintf(fptr, "5\nintermediate\ninttterrmeediateeee\nprogram\nppproograaamm\naaa");
+  // fclose(fptr);
   if (!fptr) {
-    printf("no file exists\n");
+    printf("file open error\n");
     return -1;
   }
+  
   printf("fptr exists\n");
-  int size = fscanf(fptr, " %d", &size);
+  int size = -1;
+  fscanf(fptr, " %d", &size);
+
   printf("size of file is %d\n", size);
   
-  read_file(fptr, words, 5);
+  read_file(fptr, words, size);
+  fclose(fptr);
     return 0;
 }
