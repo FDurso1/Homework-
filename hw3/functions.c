4ef941854757d1eaf4dfdc37cab6397701da87c4
Francis <fdurso1@jh.edu>
2021-02-21 14:19:33 -0500

    regex bug fixed somehow, am now working on match. Several sub-functions made, word length identifiable. Updated read_file to use strings instead of chars after Piazza response.

--- hw3/functions.c
+++ hw3/functions.c
@@ -6,41 +6,84 @@
 int read_file(FILE *fp, char words[][MAX_WORD_SIZE + 1], int size) {
   printf("read_file start\n");
   
-  char cur = 'w';
-  
-  //get rid of first line
-  while (fscanf(fp, "%c", &cur) != 0) {
-      if (cur == '\n') {
-	break;
-      }
-  }
+  char curr[MAX_WORD_SIZE+1]= "";
+  fgets(curr, MAX_WORD_SIZE+1, fp);
+  //get rid of first line (which originally contained the number of lines)
   
   
   for (int i = 0; i < size; i++) {
     printf("Word %d:\n", i);
-    int j = 0;
-    while (fscanf(fp, "%c", &cur) != 0) {
-	printf("%c", cur);
-	words[i][j] = cur;
-	if (cur == '\n') {
-	  break;
-	  //end of word found
-	}
-	j++;
-	//cur = 0;
+    fgets(curr, MAX_WORD_SIZE+1, fp);
+    printf("curr: %s\n", curr);
+
+    for (int j = 0; j < MAX_WORD_SIZE && curr[j] != '\0'; j++) {
+      words[i][j] = curr[j];
     }
   }
   return 0;
 }
 
 
+int getWordSize(const char *word) {
+  int i = 0;
+  for (i = 0; i < MAX_WORD_SIZE +1 && word[i] != '\0'; i++){}
+  return i;
 
+}
+
+int simpleMatch(const char *regex, const char *word);
 
 int match(const char *regex, const char *word, int restriction) {
     // TODO: Implement me!
   printf("match start\n");
+  //  int toReturn = 1;
+  
+  //if the regex expression is just a word in the list
+  if (simpleMatch(regex, word) == 1) {
+    return 1;
+  }
+  int wordSize =  getWordSize(word);
+  printf("The size of the word being assessed is %d\n", wordSize);
+
+  
+  for (int i = 0; i < MAX_WORD_SIZE && regex[i] != '\0'; i++) {
+    
+  }
+
+  
   printf(" %s %s %d\n", regex, word, restriction);
   return 0;
 }
+    /*
+int star(const char *regex, const char *word) {
 
+  return 0;
+}
 
+int question(const char *regex, const char *word) {
+
+  return 0;
+}
+    
+
+int tilde(const char *regex, const char *word, int restriction) {
+  
+  return 0;
+}
+    */
+
+/*
+  This method is used to check whether the regex expression is just a 
+ */
+int simpleMatch(const char* regex, const char* word) {
+  for(int i = 0; i < MAX_WORD_SIZE+1; i++) {
+    if (regex[i] != word[i]) {
+      return 0;
+    }
+  }
+  return 1;
+}
+    
+int isSorQ(char c) {
+  return (c == '*' || c == '?' || c == '~');
+} 
--- hw3/hw3.c
+++ hw3/hw3.c
@@ -36,37 +36,36 @@ int main(int argc, const char *argv[]) {
   
   read_file(fptr, words, size);
   fclose(fptr);
-  printf("testing the words array\n");
+  /*  printf("testing the words array\n");
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < MAX_WORD_SIZE +1 && words[i][j] != '\n'; j++) {
       printf("%c", words[i][j]);
     }
     printf("\n");
-  }
+    } */
   char hold = '0';
   int input = 0;
   int z = 0;
-  char reggy[MAX_WORD_SIZE] = "\0";
+  char reggy[MAX_WORD_SIZE+1] = "\0";
   while ((input = scanf(" %c", &hold)) == 1) {
      reggy[z] = hold;
      z++;
   }
-  printf("reggex expression: ");
-  for (int i = 0; i < z; i++) {
+   printf("reggex expression: ");
+   /* for (int i = 0; i < z; i++) {
     printf("%c", reggy[i]);
   }
-  printf("\n");
+  printf("\n"); */
   printf("%s\n", reggy);
   
   for (int i = 0; i < size; i++) {
-    char curWord[] = "";
+    char curWord[MAX_WORD_SIZE+1] = "";
     for (int j = 0; i < MAX_WORD_SIZE+1 && words[i][j] != '\n'; j++) {
       curWord[j] = words[i][j];
       curWord[j+1] = '\0';
     }
     printf("testing whether %s is found by %s\n", curWord, reggy);
     match(reggy, curWord, 10);
-    //reggy is not being conveyed properly
   }
 
 
