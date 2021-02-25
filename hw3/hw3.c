6600baf99884ddf1a9b8a946209efef5f552d47f
Francis <fdurso1@jh.edu>
2021-02-24 23:22:14 -0500

    Gradescope didn't like reading user input character by character, trying with a string

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -37,22 +37,20 @@ int main(int argc, const char *argv[]) {
   read_file(fptr, words, size);
   fclose(fptr);
   
-  char hold = '0';
-  int input = 0;
-  int index = 0;
-  char reggy[MAX_INPUT_SIZE+1] = "\0";
+  char reggy[MAX_INPUT_SIZE+1];
 
-  while ((input = scanf(" %c", &hold)) == 1) {
-     reggy[index] = hold;
-     index++;
+  if (scanf("%s", reggy) != 1) {
+    return 0;
   }
   int aSuccess = 0;
   for (int i = 0; i < size; i++) {
     char curWord[MAX_WORD_SIZE+1] = "";
-    for (int j = 0; i < MAX_WORD_SIZE+1 && words[i][j] != '\n'; j++) {
+    for (int j = 0; j < MAX_WORD_SIZE+1 && words[i][j] != '\n'; j++) {
       curWord[j] = words[i][j];
       curWord[j+1] = '\0';
     }
+
+    
     if( match(reggy, curWord, newRes)) {
       aSuccess = 1;
       printf("%s\n", curWord);
